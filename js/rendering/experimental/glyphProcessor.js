eafd0d146e1dc2c150ab6c17c8dec9dfedfea89b
sventral <32577096+sventral@users.noreply.github.com>
2025-11-05 11:34:10 +0100

    Update glyphProcessor.js

--- js/rendering/experimental/glyphProcessor.js
+++ js/rendering/experimental/glyphProcessor.js
@@ -1,3 +1,5 @@
+//js/rendering/experimental/glyphProcessor.js
+
 import { clamp01 } from './textureMath.js';
 import { createDistanceMapProvider } from './distanceMaps.js';
 import { createExperimentalStagePipeline, GLYPH_PIPELINE_ORDER } from './stagePipeline.js';
